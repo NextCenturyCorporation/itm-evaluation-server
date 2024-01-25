6b449db583eb6ba9bfd8306be7ebfc591f90d16a
Darren Gemoets <darren.gemoets@caci.com>
2024-01-25 18:05:29 -0500

    Fleshed out ITMScene for scene storage instead of Scene model object; removed probe system, reader, and yamls

--- swagger_server/itm/itm_scene.py
+++ swagger_server/itm/itm_scene.py
@@ -1,9 +1,36 @@
 from dataclasses import dataclass
 from typing import List, Dict
 from swagger_server.models.scene import Scene
+from swagger_server.models.action import Action
+from swagger_server.models.action_type_enum import ActionTypeEnum
+from swagger_server.models.conditions import Conditions
+from swagger_server.models.semantic_type_enum import SemanticTypeEnum
+from swagger_server.models.state import State
 
+@dataclass
+class ActionMapping:
+    """Class to represent a mapping from action to probe response."""
+    action_id: str
+    action_type: str
+    unstructured: str
+    character_id: str
+    probe_id: str
+    choice: str
+    kdma_association :Dict[str, float]
+    conditions :Conditions
+    next_scene :int
 
- 
+    def __init__(self, action :Action, scene_index :int):
+        self.action_id = action.action_id
+        self.action_type = action.action_type
+        self.unstructured = action.unstructured
+        self.character_id = action.character_id
+        self.probe_id = action.probe_id
+        self.choice = action.choice
+        self.kdma_association = action.kdma_association
+        self.conditions = action.conditions
+        self.next_scene = scene_index+1 if action.next_scene == None else action.next_scene
+
 class ITMScene:
     """
     Class for managing a scene in the ITM system.
@@ -13,4 +40,13 @@ class ITMScene:
         """
         Initialize an instance of ITMScene.
         """
-        self.scene :Scene = scene
+        self.index = scene.index
+        self.state :State = scene.state
+        self.end_scene_allowed = scene.end_scene_allowed
+        self.action_mappings :List[ActionMapping] = [
+            ActionMapping(action_mapping, self.index)
+            for action_mapping in scene.action_mapping
+        ]
+        self.restricted_actions :List[ActionTypeEnum] = scene.restricted_actions
+        self.transition_semantics :SemanticTypeEnum = scene.transition_semantics
+        self.transitions :Conditions = scene.transitions
--- swagger_server/itm/itm_session_scenario_object.py
+++ swagger_server/itm/itm_session_scenario_object.py
@@ -1,10 +1,7 @@
-from time import time
 from typing import List
 from dataclasses import dataclass
-from datetime import datetime
 from swagger_server.models import Scenario
 from .itm_character_simulator import ITMCharacterSimulator, CharacterSimulation
-from .itm_probe_reader import ITMProbeReader
 from .itm_scenario_reader import ITMScenarioReader
 from .itm_scene import ITMScene
 from .itm_alignment_target_reader import ITMAlignmentTargetReader
@@ -14,8 +11,6 @@ from .itm_ta1_controller import ITMTa1Controller
 class ITMSessionScenarioObject:
     scenario: Scenario = None
     scenes: List[ITMScene] = None
-    scenario_reader: ITMScenarioReader = None
-    probe_system: ITMProbeReader = None
     alignment_target_reader: ITMAlignmentTargetReader = None
     character_simulations: List[CharacterSimulation] = None
     character_simulator: ITMCharacterSimulator = None
@@ -33,23 +28,14 @@ class ITMSessionScenarioObjectHandler:
         # isso is short for ITM Session Scenario Object
         isso = ITMSessionScenarioObject()
 
-        isso.scenario_reader = ITMScenarioReader(self.yaml_path + "scenario.yaml")
-        isso.probe_system = ITMProbeReader(self.yaml_path)
-        isso.probe_system.scenario = isso.scenario
+        scenario_reader = ITMScenarioReader(self.yaml_path + "scenario.yaml")
         ( isso.scenario, isso.scenes, isso.character_simulations) = \
-            isso.scenario_reader.read_scenario_from_yaml()
-        print(isso.scenario)
-        print('-------------')
-        print(isso.scenes[0].scene)
-        print('-------------')
-        print(isso.scenes[1].scene)
+            scenario_reader.read_scenario_from_yaml()
         isso.character_simulator = ITMCharacterSimulator()
         isso.character_simulator.setup_characters(isso.scenario, isso.character_simulations)
 
         if not self.training:
             isso.alignment_target_reader = ITMAlignmentTargetReader(self.yaml_path + "alignment_target.yaml")
-
-        isso.scenario.start_time = datetime.fromtimestamp(time()).strftime("%Y-%m-%d %H:%M:%S.%f")
 
         isso.ta1_controller = ITMTa1Controller(
             isso.alignment_target_reader.alignment_target.id if not self.training else None,
