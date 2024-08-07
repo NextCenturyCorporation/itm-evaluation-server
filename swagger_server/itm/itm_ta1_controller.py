efffb8f0cfba83646970287b6974a82ba267c363
Ayal-Ciobotaru <91137452+Ayal-Ciobotaru@users.noreply.github.com>
2024-08-07 09:55:14 -0400

    ITM-536 (#98)
    * created host_port combo configs, adjusted readme, and more based on PR comments
    
    * Update to latest ADEPT AWS IP

--- swagger_server/itm/itm_ta1_controller.py
+++ swagger_server/itm/itm_ta1_controller.py
@@ -1,42 +1,41 @@
 import requests
 import json
-import os
 import urllib
 from swagger_server.models.probe_response import ProbeResponse  # noqa: F401,E501
 from swagger_server.models.alignment_results import AlignmentResults  # noqa: F401,E501
 from swagger_server.models.alignment_target import AlignmentTarget  # noqa: F401,E501
+from swagger_server import config_util
+
 
 class ITMTa1Controller:
-    ADEPT_PORT = os.getenv("ADEPT_PORT")
-    if (ADEPT_PORT is None or ADEPT_PORT == ""):
-        ADEPT_PORT = '8081'
-    SOARTECH_PORT = os.getenv("SOARTECH_PORT")
-    if (SOARTECH_PORT is None or SOARTECH_PORT == ""):
-        SOARTECH_PORT = '8084'
-
-    def __init__(self, alignment_target_id, scene_type, alignment_target = None):
+    config_util.check_ini()
+    config = config_util.read_ini()[0]
+    ADEPT_URL = config['DEFAULT']['ADEPT_URL']
+    SOARTECH_URL = config['DEFAULT']['SOARTECH_URL']
+    
+    def __init__(self, alignment_target_id, scene_type, config, alignment_target = None):
         self.session_id = ''
         self.alignment_target_id = alignment_target_id
         self.alignment_target = alignment_target
-        self.host, self.port = ITMTa1Controller.get_contact_info(scene_type=scene_type)
+        self.url = ITMTa1Controller.get_contact_info(scene_type=scene_type)
 
     @staticmethod
     def get_contact_info(scene_type):
-        port = ITMTa1Controller.ADEPT_PORT if scene_type == 'adept' else ITMTa1Controller.SOARTECH_PORT
-        host =  os.getenv("ADEPT_HOSTNAME") if scene_type == 'adept' else os.getenv("SOARTECH_HOSTNAME")
-        if host is None or host == "":
-            host = "localhost"
-        return host, port
+        host_port = ITMTa1Controller.ADEPT_URL if scene_type == 'adept' else ITMTa1Controller.SOARTECH_URL
+        # Technically this `if` block should never be hit since configs are mandatory but just in case
+        if host_port is None or host_port == "":
+            host_port = "localhost"
+        return host_port
 
     @staticmethod
     def get_alignment_data(scene_type):
-        (host, port) = ITMTa1Controller.get_contact_info(scene_type=scene_type)
+        host_port = ITMTa1Controller.get_contact_info(scene_type=scene_type)
         target_id_path = 'alignment_target_ids' if scene_type == 'adept' else 'alignment_targets'
-        url = f"http://{host}:{port}/api/v1/{target_id_path}"
+        url = f"{host_port}/api/v1/{target_id_path}"
         alignment_target_ids = json.loads(requests.get(url).content.decode('utf-8'))
         alignments = []
         for alignment_target_id in alignment_target_ids:
-          url = f"http://{host}:{port}/api/v1/alignment_target/{alignment_target_id}"
+          url = f"{host_port}/api/v1/alignment_target/{alignment_target_id}"
           alignment_target = json.loads(requests.get(url).content.decode('utf-8'))
           alignments.append(AlignmentTarget.from_dict(alignment_target))
         return alignments
@@ -45,7 +44,7 @@ class ITMTa1Controller:
         return json.loads(response.content.decode('utf-8'))
 
     def new_session(self, user_id=None):
-        url = f"http://{self.host}:{self.port}/api/v1/new_session"
+        url = f"{self.url}/api/v1/new_session"
         if user_id:
             params = {"user_id": user_id}
             url = f"{url}?{urllib.parse.urlencode(params)}"
@@ -57,12 +56,12 @@ class ITMTa1Controller:
 
     def post_probe(self, probe_response: ProbeResponse):
         body = {"session_id": self.session_id, "response": probe_response.to_dict()}
-        url = f"http://{self.host}:{self.port}/api/v1/response"
+        url = f"{self.url}/api/v1/response"
         self.to_dict(requests.post(url, json=body))
         return None
     
     def get_probe_response_alignment(self, scenario_id, probe_id):
-        base_url = f"http://{self.host}:{self.port}/api/v1/alignment/probe"
+        base_url = f"{self.url}/api/v1/alignment/probe"
         session_id = self.session_id
         params = {
             "session_id": session_id,
@@ -77,7 +76,7 @@ class ITMTa1Controller:
         return response
 
     def get_session_alignment(self, target_id = None):
-        base_url = f"http://{self.host}:{self.port}/api/v1/alignment/session"
+        base_url = f"{self.url}/api/v1/alignment/session"
         params = {
             "session_id": self.session_id,
             "target_id": self.alignment_target_id if not target_id else target_id
